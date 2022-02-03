f7aa6dfe70f7a31b3b631a73bc36d243224ff6a6
mikseros <mmadynski@gmail.com>
2022-02-03 11:29:17 +0100

    Save Employee REST API created

---
+++ src/main/java/com/mikseros/springboot/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.mikseros.springboot.service;
+
+import com.mikseros.springboot.model.Employee;
+
+public interface EmployeeService {
+	Employee saveEmployee(Employee employee);
+}
---
+++ src/main/java/com/mikseros/springboot/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,24 @@
+package com.mikseros.springboot.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.mikseros.springboot.model.Employee;
+import com.mikseros.springboot.repository.EmployeeRepository;
+import com.mikseros.springboot.service.EmployeeService;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+	private EmployeeRepository employeeRepository;
+	
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+		super();
+		this.employeeRepository = employeeRepository;
+	}
+
+	@Override
+	public Employee saveEmployee(Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+}
